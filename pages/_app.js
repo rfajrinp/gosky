c3c2b7c797df2889e71f53e11c355b28791ae7c2
rfajrinp <97358900+rfajrinp@users.noreply.github.com>
2022-12-30 19:16:22 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,11 @@
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import { AppWrapper } from "./context/AppContext";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <AppWrapper>
         <Component {...pageProps} />
-      </AppWrapper>
     </div>
   );
 }
